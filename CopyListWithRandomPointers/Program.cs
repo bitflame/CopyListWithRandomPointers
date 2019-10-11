0e9b58e2d88edc972a0443d27ade5ac16aff61fb
Liquid Falme <sansari1@cox.net>
2019-10-11 12:08:08 -0400

    The algorithm with no extra space seems to produce the right answer, but it fails for the test case of just one node. I need to add exceptions I think.

--- CopyListWithRandomPointers/Program.cs
+++ CopyListWithRandomPointers/Program.cs
@@ -18,11 +18,17 @@ namespace CopyListWithRandomPointers
             secNode.random = secNode;
             secNode.next = null;
             head = firstNode;
-            PrintNode(cloneExtraSpace(head));
+            PrintNode(cloneNoExtraSpace(head));
         }
         private static void PrintNode(Node n)
         {
             Console.WriteLine("{0}{1}{2}", n.val, n.next, n.random);
+            Console.ReadLine();
+        }
+        private static void CompareTwoNodes (Node n1, Node n2)
+        {
+            Console.WriteLine("The first node: {0}{1}{2}",n1.val, n1.next, n1.random);
+            Console.WriteLine("The second node: {0}{1}{2}", n2.val, n2.next, n2.random);
             Console.ReadLine();
         }
         private static Node cloneExtraSpace(Node n)
@@ -45,21 +51,49 @@ namespace CopyListWithRandomPointers
             }
             nCurr = n;
             copyCurr = copy;
-            while (copyCurr != null)
+            while (nCurr != null)
             {
-                mapping.TryGetValue(nCurr.random, out Node tempNode);
-                copyCurr.random = tempNode;
+                //mapping.TryGetValue(nCurr.random, out Node tempNode);
+                //copyCurr.random = tempNode;
+                //I thought the problem might be with the above two lines so I changed it to what is below
+                copyCurr.random = mapping[nCurr.random];
                 nCurr = nCurr.next;
                 copyCurr = copyCurr.next;
             }
             return copy;
         }
-        //private static Node cloneNoExtraSpace(Node n)
-        //{
-        //    if ( n == null)
-        //    {
-        //        return null;
-        //    }
-        //}
+        private static Node cloneNoExtraSpace(Node n)
+        {
+            if (n == null)
+            {
+                return null;
+            }
+            Node nCurr = n;//This line and the line below seem to give the exact same result
+            //Node tempNode = new Node(n.val, n.next, n.random);
+            //CompareTwoNodes(nCurr, tempNode);
+            while (nCurr != null)
+            {
+                Node temp = new Node();
+                temp.val = nCurr.val;
+                temp.next = nCurr.next;
+                nCurr.next = temp;
+                nCurr = nCurr.next.next;
+            }
+            nCurr = n;
+            while (nCurr != null)
+            {
+                nCurr.next.random = nCurr.random.next;
+                nCurr = nCurr.next.next;
+            }
+            Node copy = n.next;
+            nCurr = n;
+            while (nCurr.next != null)
+            {
+                Node temp = nCurr.next;
+                nCurr.next = nCurr.next.next;
+                nCurr = temp;
+            }
+            return copy;     
+        }
     }
 }
